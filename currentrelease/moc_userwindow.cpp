35eabb9efa46089293a21dae20e15a8089e251a9
chrisgassen <chrisgassen@hotmail.de>
2019-07-11 17:51:36 +0200

    trying to get software pi-ready

--- currentrelease/moc_userwindow.cpp
+++ currentrelease/moc_userwindow.cpp
@@ -6,7 +6,7 @@
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
-#include "../../till-pm-tnmjc/src/userwindow.h"
+#include "../../pm-tnmjc/src/userwindow.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
